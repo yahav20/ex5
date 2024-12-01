e883cf893990c3069c55b67d54a97ee9f69b98b4
yahav20 <yahavezer256@gmail.com>
2024-12-02 00:12:29 +0200

    adding mutex

--- trying_theards.cpp
+++ trying_theards.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <thread>
 #include <unistd.h>
+#include <mutex>
 
 using namespace std;
 
+mutex mtx; //using mutex (to lock theards)
+
 void foo(int a) {   
-    sleep(5);
+    mtx.lock(); // lock entring new threads
     cout << a << endl;
+    mtx.unlock(); // realse lock after thread finish to print
 }
 
 int main() {
